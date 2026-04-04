940e928c1030100f8e5a40c4066354b8f5f756d6
timurkodiorvaslonovich <timurkodirovdeveloper@gmail.com>
2026-04-04 09:45:15 +0500

    adding security jwt tokenized based on USER/ADMIN roles

---
+++ src/main/java/com/tim/movie_booking/controller/OwnUserController.java
@@ -0,0 +1,33 @@
+package com.tim.movie_booking.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tim.movie_booking.dto.UserResponseDto;
+import com.tim.movie_booking.repository.UserRepository;
+import com.tim.movie_booking.service.UserService;
+import com.tim.movie_booking.entity.User;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class OwnUserController {
+    private final UserRepository userRepository;
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+
+
+    @GetMapping("/me")
+    public ResponseEntity<UserResponseDto> getMyProfile(
+        @AuthenticationPrincipal User currentUser
+     ) {
+        return ResponseEntity.ok(userService.toDto(currentUser));
+     }
+
+}
